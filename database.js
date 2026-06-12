19eab0ee1589b16e7e098095b421975fa2f44130
ems-voters <luke.tinikashvili1621@gmail.com>
2026-06-12 20:24:27 +0400

    Update database.js

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const DATA = [
         lastName: "თინიკაშვილი",
         firstName: "ლუკა",
         surname: "თინიკაშვილი",
-        birth: "16.01.2003",
+        birth: "16.01.2008",
         station: "09.60",
         stationType: "ელექტრონული",
         mapUrl: "https://maps.google.com",
